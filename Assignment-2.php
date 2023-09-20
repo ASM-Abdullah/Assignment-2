8207ce64e93a59b76f70b1af80a14db782c8d7c8
ASM-Abdullah <abdullah3010@yahoo.com>
2023-09-20 22:45:57 +0600

    Formatting done

--- Assignment-2.php
+++ Assignment-2.php
@@ -7,12 +7,11 @@ function to print.
 Also do the same using while loop and do-while loop also. */
 
 
-// Solution of Task-1:
+               // Solution of Task-1:
 echo "\n\n";
-echo "====> Task-1\n(Using For Loop)\n";
+echo "==> Task-1===>\n(Using For Loop)=====>\n\n";
 
-function printAlternateEvenNumbers($start, $end, $step)
-{
+function printAlternateEvenNumbers(int $start, int $end, int $step){
   $evenNumbers = [];
   for ($i = $start; $i <= $end; $i += 1) { // Using "for loop" to get even numbers.
     if ($i % 2 == 0) {
@@ -21,16 +20,16 @@ function printAlternateEvenNumbers($start, $end, $step)
   }
   // Using another "for loop" to get every alternate even number.
   for ($i = 0; $i < count($evenNumbers); $i += $step) {
-    echo $evenNumbers[$i] . "\n";
+    echo $evenNumbers[$i] ." ". "\n";
   }
 }
 
 printAlternateEvenNumbers(1, 20, 2);
 
-echo "\n\n";
-echo "=> Task-1\n(Using While Loop)\n";
+echo "\n\n<br>";
+echo "====> Task-1=>\n(Using While Loop)===>\n\n";
 
-function printAlternateEvenNumbers1($start, $end, $step){
+function printAlternateEvenNumbers1(int $start, int $end, int $step){
   $i = $start;
   // If $start is odd, increment it to the next even number
   if ($i % 2 != 0) {
@@ -38,7 +37,7 @@ function printAlternateEvenNumbers1($start, $end, $step){
   }
   while ($i <= $end) {
     if ($i % 2 == 0) {
-      echo $i . "\n";
+      echo $i . " " . "\n";
     }
     $i += $step * 2; //to skip odd numbers
   }
@@ -46,9 +45,9 @@ function printAlternateEvenNumbers1($start, $end, $step){
 
 printAlternateEvenNumbers1(1, 20, 2);
 
-echo "\n\n";
-echo "=> Task-1\n(Using Do-while Loop)\n";
-function printAlternateEvenNumbers2($start, $end, $step)
+echo "\n\n<br>";
+echo "====> Task-1=>\n(Using Do-while Loop)=>\n\n";
+function printAlternateEvenNumbers2(int $start, int $end, int $step)
 {
   $i = $start;
   if ($i % 2 != 0) {
@@ -56,7 +55,7 @@ function printAlternateEvenNumbers2($start, $end, $step)
   }
   do {
     if ($i % 2 == 0) {
-      echo $i . "\n";
+      echo $i . " " . "\n";
     }
     $i += $step * 2;    // to skip odd numbers
   } while ($i <= $end);
@@ -64,29 +63,31 @@ function printAlternateEvenNumbers2($start, $end, $step)
 
 printAlternateEvenNumbers2(1, 20, 2);
 
-echo "\n\n";
-echo "====> Task-2\n";
+
+echo "\n\n<br>";
+echo "==> Task-2==>\n\n";
 // Task 2: Skip Multiples of 5
 /* Create a PHP script that prints numbers from 1 to 50 using a for loop. However, when the
 loop encounters a multiple of 5, it should skip that number using the continue statement and
 continue to the next iteration. */
 
+               // Solution of Task-2:
 for ($i = 1; $i <= 50; $i++) {   // For loop initialized from 1 and continued upto 50.
   if ($i % 5 == 0) {    //IF any number is a multiple of 5, it will be skipped.
     continue;
   }
-  echo $i . "\n";
+  echo $i . " " . "\n";
 }
 
 
-echo "\n\n";
-echo "====> Task-3\n";
+echo "\n\n<br>";
+echo "==> Task-3==>\n\n";
 // Task 3: Break on Condition
 /* Write a PHP program that calculates and prints the first 10 Fibonacci numbers. But, if a
 Fibonacci number is greater than 100, break out of the loop using the break statement.
  */
 
-
+               // Solution of Task-3:
 $totalFibonacciNumbers = 10;  //As the first 10 Fibonacci numbers are wanted
 $old = 0;
 $next = 1;
@@ -95,14 +96,14 @@ for ($i = 0; $i < $totalFibonacciNumbers; $i++) {
   if ($old > 100) {
     break;            // Use of break statement (for any number greater than 100, if encountered) in "for loop".
   }
-  echo $old . "\n";
+  echo $old . " " . "\n";
   $next = $newest;
   $newest = $old + $next;
   $old = $next;
 }
 
-echo "\n\n";
-echo "====> Task-4\n";
+echo "\n\n<br>";
+echo "==> Task-4==>\n\n";
 
 
 
@@ -111,7 +112,7 @@ echo "====> Task-4\n";
 this 15 as an argument of a function and use a for loop to generate these numbers and print
 them by calling the function. */
 
-
+               // Solution of Task-4:
 function fibonacci(int $totalNum = 15)
 {
   $old = 0;
@@ -119,7 +120,7 @@ function fibonacci(int $totalNum = 15)
   $newest = 1;
   for ($i = 0; $i < $totalNum; $i++) {
   
-    echo $old . "\n";  //Printing the first 15 numbers.
+    echo $old . " " . "\n";  //Printing the first 15 numbers.
     $next = $newest;
     $newest = $old + $next;
     $old = $next;
